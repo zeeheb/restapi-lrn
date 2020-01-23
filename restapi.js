9e0352dbff26cfee513076ff8d26f818cda02027
José Eduardo Brandão <39067212+zeeheb@users.noreply.github.com>
2020-01-23 09:41:31 -0300

    remove spinner

--- restapi.js
+++ restapi.js
@@ -1,27 +1,38 @@
+const spinner = document.createElement('img');
+
 loadEventListener();
 
 function loadEventListener() {
-  document.addEventListener('DOMContentLoaded', fazSpinner);
+  document.addEventListener('DOMContentLoaded', fazSpinner(spinner));
 }
 
-function fazSpinner() {
-  const spinner = document.createElement('img');
-
+function fazSpinner(spinner) {
+  // const spinner = document.createElement('img');
+  spinner.classList.add('spinner');
   spinner.src =
     'https://mir-s3-cdn-cf.behance.net/project_modules/disp/3c215736197347.57135ca123427.gif';
   document.querySelector('body').appendChild(spinner);
 }
 
-fetch(
-  'https://servicodados.ibge.gov.br/api/v1/localidades/municipios/3550307/distritos/',
-  {
-    method: 'GET'
-  }
-)
-  .then(resp => {
-    fazSpinner();
-    return resp.json();
-  })
-  .then(result => {
-    alert('Fetched');
-  });
+function remSpinner(spinner) {
+  spinner.parentNode.removeChild(spinner);
+}
+
+setTimeout(requestData, 1000);
+
+function requestData() {
+  fetch(
+    'https://servicodados.ibge.gov.br/api/v1/localidades/municipios/3550304/distritos/',
+    {
+      method: 'GET'
+    }
+  )
+    .then(resp => {
+      // fazSpinner();
+      return resp.json();
+    })
+    .then(result => {
+      remSpinner(spinner);
+      // alert('Fetched');
+    });
+}
